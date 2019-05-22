c4d9e4a7424e60cf09b7180268be8f78449c8892
achil <achil@localhost.localdomain>
2019-05-22 12:25:29 +0100

    add public to gitignore

--- imports/ui/components/Main.js
+++ imports/ui/components/Main.js
@@ -8,42 +8,46 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading:true
+            isLoading: true
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         if (this.state.isLoading && Array.isArray(this.props.funnels) && this.props.funnels.length) {
             setTimeout(() => {
                 this.setState({ isLoading: false });
             }, 500);
-        } 
+        }
     }
 
     componentWillReceiveProps(nextProps) {
-        if (this.state.isLoading && Array.isArray(nextProps.funnels)&& nextProps.funnels.length ) {
-                setTimeout(() => {
+        if (this.state.isLoading && Array.isArray(nextProps.funnels) && nextProps.funnels.length) {
+            setTimeout(() => {
                 this.setState({ isLoading: false });
-                }, 500);
+            }, 500);
         }
     }
 
     render() {
-        const{params, funnels, search, categories,userId, industries}=this.props;
-        const {isLoading}= this.state;
+        const { params, funnels, search, categories, userId, industries } = this.props;
+        const { isLoading } = this.state;
         return (
-                <div className="wrapper wrapper-content">
-                    <div className="row">
-                        <div className="col-lg-3">
-                            <FilterFunnelMenu {...this.props} search={search} funnels={funnels} params={params} industries={industries} categories={categories} />
-                        </div>
-                        <div className="col-lg-9 animated fadeInRight">
-                           {isLoading?<div style={{minHeight:'200px'}} > <div className="overlay-funnels" ><i className="fa fa-gear fa-spin"></i></div></div>:<div className="row">
+            <div className="wrapper wrapper-content">
+                <div className="row">
+                    <div className="col-lg-3">
+                        <FilterFunnelMenu {...this.props} search={search} funnels={funnels} params={params} industries={industries} categories={categories} />
+                    </div>
+                    <div className="col-lg-9 animated fadeInRight">
+                        {isLoading ?
+                            <div style={{ minHeight: '200px' }} > <div className="overlay-funnels" ><i className="fa fa-gear fa-spin"></i></div></div>
+                            :
+                            <div className="row">
                                 <FunnelList funnels={this.props.funnels} />
-                            </div>}
-                        </div>
+                            </div>
+                        }
                     </div>
                 </div>
+            </div>
         )
     }
 }
