45c5c268454ef5bd2c8f5617c85e0ee1e009cedb
Kevin-it237 <ngaleuabel@gmail.com>
2019-08-17 16:37:25 +0100

    Put manual in English

--- imports/ui/pages/help/UserHelpPage.js
+++ imports/ui/pages/help/UserHelpPage.js
@@ -18,26 +18,26 @@ class UserHelpPage extends Component {
           <div className="container">
             <div className="row m-t-5 full-content">
               <div className="col">
-                  <h2 className="text-center title">Manuel d’utilisation</h2><hr/>
+                  <h2 className="text-center title">Manual for Simple User</h2><hr/>
                   <div className="col-sm-12 content">
-                    <h5 className="step text-center">Présentation du système</h5>
-                    <p>Tontinards est une plate-forme Web, qui met en relation des personnes ayant des projets professionnels ou personnels et des personnes ayant des capacités de financement. L'application enregistre les données de projet soumises par les demandeurs de financement dans la base de données.</p>
+                    <h5 className="step text-center">System Overview</h5>
+                    <p>Tontinards is a platform web, which connects people who have professional or personal projects and people who have financing capabilities.The application saves project data submitted by the funding applicants in the database.</p>
                   </div>
                   <div className="col-sm-12 content">
-                      <h5 className="step">2. DESCRIPTION DE CHAQUE CAS D'UTILISATION POUR UN UTILISATEUR SIMPLE</h5>
-                      <h6 className="">2.1 S’INSCRIRE</h6>
-                      <p><strong>En tant qu'utilisateur simple, vous souhaitez vous inscrire:</strong></p>
+                      <h5 className="step">2. DESCRIPTION OF EACH USE CASE FOR SIMPLE USER</h5>
+                      <h6 className="">2.1 SIGN UP</h6>
+                      <p><strong>As a simple user you want to Register :</strong></p>
                       <ul>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Cliquez sur "S'inscrire"</li>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Sur cette page, entrez votre nom d'utilisateur, votre mot de passe et votre adresse e-mail ou des connexions à des réseaux sociaux tels que Facebook, Twitter et Google, selon vos besoins.</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Click on "Register"</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> On this page, your username, your password and email or connections to social networks such as facebook twitter and google will be you requested.</li>
                       </ul>
                       <img src="/images/manual/im1.png" />
-                      <p><strong>Remarque! : </strong>Notez qu'il n'y a qu'un seul identifiant et donc qu'un seul mot de passe par compte. Pour valider, cliquez sur "Enregistrer".</p>
-                      <h6 className="">2.1 SE CONNECTER</h6>
-                      <p><strong>En tant qu'utilisateur simple, vous souhaitez vous connectez:</strong></p>
+                      <p><strong>Note! :  </strong>Note that there is only one login and therefore only one password per account. To validate click on "Save".</p>
+                      <h6 className="">2.2 SIGN IN</h6>
+                      <p><strong>As a simple user you want to Log In:</strong></p>
                       <ul>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Cliquez sur "Login"</li>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Sur cette page, votre nom d'utilisateur et votre mot de passe ou vos connexions à des réseaux sociaux tels que Facebook, Twitter et Google vous seront demandés.</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Click on "Login"</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> On this page, your username and your password or connections to social networks such as facebook twitter and google will be you requested.</li>
                       </ul>
                       <img src="/images/manual/im2.png" />
                   </div>
@@ -45,41 +45,39 @@ class UserHelpPage extends Component {
 
                   <div className="col-sm-12 content">
                       <h5 className="step">3. MENU UTILISATEUR</h5>
-                      <p>Maintenant que vous êtes connecté, vous entrez dans l'application Web "Tontinards". Nous allons décrire toutes les fonctionnalités de l'application après les autres. Dès que vous vous connectez, à droite, un onglet à
-                        nom de votre compte, dans cet exemple <b>"Waffo"</b>: <b>"Mes campagnes"</b>, <b>"Profil"</b>, <b>"Créer un projet"</b> et <b>"Déconnexion"</b> nous détaillerons chaque fonctionnalité a la suite:
+                      <p>Now that you are logged in, you enter the "Tontinards" web application. We will describe all the features of the application after others. As soon as you connect, On the right is a tab at name of your account, in this example <b>"Waffo"</b>: <b>"My Campaigns"</b>, <b>"Profile"</b>, <b>"Create Project"</b> and <b>"Logout"</b> that we will discuss later.
                       </p>
                       <img src="/images/manual/im3.png" />
-                      <p>En cliquant dessus, vous ouvrez une boîte de dialogue avec 4 possibilités:</p>
+                      <p>Clicking on it opens a dialog box with 4 possibilities:</p>
                       <ul>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Mes campagnes": </b> &nbsp;Vous permet de retrouver le statut de toutes vos campagnes en cours.</li>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Profil": </b> &nbsp;Permet de modifier les paramètres "Utilisateur"</li>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Créer un projet": </b> &nbsp;Vous permet de créer un projet qui sera validé et accepté par un administrateur pour qu'il passe a l’état d'une campagne. </li>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Déconnexion": </b> &nbsp;Vous permet de vous déconnecter </li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "My Campaigns": </b> &nbsp;Vous permet de retrouver le statut de toutes vos campagnes en cours.</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Profile": </b> &nbsp;Allows you to change the "User" settings</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Create Project": </b> &nbsp;Allows you to create a project that will be valid and accepted by an administrator for it to be a campaign </li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /><b> "Logout": </b> &nbsp;Allows you to disconnect</li>
                       </ul>
-                      <h6 className="">3.1 CREER UN PROJET</h6>
-                      <p><strong>En tant qu'utilisateur simple, vous souhaitez créer un projet:</strong></p>
+                      <h6 className="">3.1 CREATE PROJECT</h6>
+                      <p><strong>As a simple user you want to Create Project:</strong></p>
                       <ul>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> En vous connectant, vous devez créer votre projet. Pour ce faire, cliquez sur "Faire un Don" sur la page de destination:</li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> By logging in you have to create your project. To do this, click on "Get Funded" on the landing page:</li>
                         <img src="/images/manual/im4.png" />
                       </ul>
                       <ul>
-                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Formulaire de création de projet: </li>
+                        <li><FontAwesomeIcon icon={faChevronRight} size={"1x"} /> Project creation form </li>
                       </ul>
-                      <p>Toutes les informations ne sont pas obligatoires mais plus votre projet contient suffisamment d'informations, plus il sera facile pour l'administrateur de le comprendre et de le valider. N'hésite pas par exemple, pour ajouter une description complète. Vous devez également choisir (Nom du projet, Numéro de téléphone, Image du projet de catégorie, Document du projet et Vidéo du projet) comme champ obligatoire. Pour continuer, cliquez sur "enregistrer".</p>
+                      <p>All information is not mandatory but more your project has enough information, the easier it will be for the administrator to better understand the project and validate it. Do not hesitate for example, to add a complete description. You must also choose (Project Name, Phone Number, Category project image, Project Document and Project Video) as a required field To continue, click on "save".</p>
                       <img src="/images/manual/im5.png" />
-                      <p><strong>Remarque!</strong> sur l'option "Funds Raise As" si l'utilisateur a sélectionné Un individu, le champ d'image du projet n'est plus disponible. Vous venez de finaliser la création de votre projet, Il apparaîtra maintenant sur l'application Web parmi les projets que vous avez soumis (projet en attente).</p>
-                      <h6 className="">3.2 MES CAMPAGNES</h6>
-                      <p><strong>En tant qu'utilisateur simple, vous souhaitez gérer vos campagnes:</strong></p>
-                      <p>Lorsque vous avez créé et géré vos projets, ceux-ci apparaissent sous forme de liste dans l'onglet "Options de filtrage" comme indiqué ci-dessous.</p>
-                      <p>Par défaut, les projets qui seront les projets "en attente". Vous pouvez accéder à d'autres projets via des filtres (projets validés ou campagnes), comme décrit ci-dessous, nous décrivons chaque état du projet.</p>
+                      <p><strong>Note! </strong> on the "Funds Raise As" option if the user selected An individual the project image field is no longer available standard. You have just finalized the creation of your project. It will now appear on the web application among your submitted projects (campaigns pending).</p>
+                      <h6 className="">3.2 MY CAMPAIGN</h6>
+                      <p><strong>As a simple user you want to Manage My Campaigns :</strong></p>
+                      <p> When you have created and managed your projects, they will appear as a list in the "Filter Options" tab as below By default, the projects that will are the "Pending" projects. You can access other projects through filters (Validated projects or Campaigns) as below we describe each state of project.</p>
                       <img src="/images/manual/im6.png" />
-                      <h6 className="">3.2.1 Projets en attente</h6>
-                      <p>Tous les projets que l'administrateur n'a pas valides sont répertoriés dans cette partie et par défaut, lorsque nous venons de créer un projet, celui-ci est positionné dans les projets "en attente".</p>
-                      <h6 className="">3.2.2 Projets validés</h6>
-                      <p>Tous les projets que l'administrateur a validés sont répertoriés dans cette partie et l'administrateur pourra effectuer des révisions pour améliorer le projet afin qu'il passe à l'état de la campagne.</p>
+                      <h6 className="">3.2.1 Pending projects</h6>
+                      <p>All the projects so the administrator has not valid are listed in this part and by default when we have just created a project it is positioned in "Pending" projects.</p>
+                      <h6 className="">3.2.2 Validated projects</h6>
+                      <p>All the projects so the administrator has validated are listed in this part and the administrator will be able to make reviews to improve the project so that it goes to the state of campaign</p>
                       <img src="/images/manual/im7.png" />
-                      <h6 className="">3.2.3 Campagnes</h6>
-                      <p>Tous les projets validé par l'administrateur  et commence une campagne sont répertoriés dans cette partie et peuvent être affichés avec les commentaires de l'administrateur pour rendre la campagne plus crédible.</p>
+                      <h6 className="">3.2.3 Campaigns</h6>
+                      <p>All the projects that the administrator has validated and starts a campaign are listed in this part and can be displayed with the admin reviews to make the campaign more credible.</p>
                       <img src="/images/manual/im8.png" />
                   </div>
               </div>
